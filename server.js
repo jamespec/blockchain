632564a3e5d453d33e151b43e7cd1d91379d25d6
james <james315@icloud.com>
2023-12-18 12:01:06 -0500

    Done playing with Express tutorial.

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
 
-app.set('view engine', 'ejs');
+app.use(express.static("public"))               // Serve static pages
+app.use(express.urlencoded({extended: true}))   // Decodes format to access elements.
+app.use(express.json())                         // Parse JSON received from the client.
+app.set('view engine', 'ejs')                   // Render pages, dynamic content.
 
-app.get('/', (req, res) => {
-    console.log("In get");
-    res.render('index', { text: "world" });
+// Create standard logging to be used when accessing all endpoints.
+// The app.use ahead of defining the routes makes this "middleware" run.
+function logger( req, res, next ) {
+    console.log( req.originalUrl )
+    next()
+}
+app.use( logger )
+
+app.get('/', logger, (req, res) => {
+    console.log("In get")
+    res.render('index', { text: "world" })
 })
 
-app.listen(3000);
+const users = require('./routes/users')
+app.use('/users', users )
+
+app.listen(3000)
